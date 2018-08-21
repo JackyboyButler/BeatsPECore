bc538351cd34dd2ac1c328c5a5a126eae58cfa9d
BlackWebPE <42561085+BlackWebPE@users.noreply.github.com>
2018-08-20 22:26:23 -0400

    Create EntityManager.php

---
+++ src/BeatsCore/entity/EntityManager.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeatsCore\entity;
+
+
+use pocketmine\entity\Entity;
+class EntityManager extends Entity{
+
+    public static function start() : void{
+    }
+}
